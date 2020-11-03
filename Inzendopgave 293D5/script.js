eff0124f9affaecc2397019dcf89bd6c4ac4f8df
xsignsx <k.j.a.koenekoop@gmail.com>
2020-11-03 18:37:40 +0100

    Add sub-menus

--- Inzendopgave 293D5/script.js
+++ Inzendopgave 293D5/script.js
@@ -1,9 +1,17 @@
-const navbarBurger = document.getElementById("navbar__burger");
-const sideDrawer = document.getElementById("side-drawer")
+const navbarBurger = document.getElementById('navbar__burger');
+const sideDrawer = document.getElementById('side-drawer');
 
 const toggleSideDrawer = () => {
-  sideDrawer.classList.toggle("side-drawer--visible");
-  document.body.classList.toggle("locked")
-}
+  sideDrawer.classList.toggle('side-drawer--visible');
+  document.body.classList.toggle('locked');
+};
+
+const dropdownMenu = document.getElementById('dropdown-menu');
+const dropdownContent = document.getElementById('dropdown-content');
+
+const toggleDropdown = () => {
+  dropdownContent.classList.toggle('visible');
+};
 
 navbarBurger.addEventListener('click', toggleSideDrawer);
+dropdownMenu.addEventListener('click', toggleDropdown);
